e806c6b2cb41e221fbe43c3c4ed5639f831736d4
Mdsoln <muddyfakih98@gmail.com>
2024-11-24 18:16:14 +0300

    added updating home page details endpoints

--- src/main/java/com/uiss/home/controller/AdminPageController.java
+++ src/main/java/com/uiss/home/controller/AdminPageController.java
@@ -2,15 +2,14 @@ package com.uiss.home.controller;
 
 import com.uiss.home.HomePageService;
 import com.uiss.home.models.HomeRequest;
+import io.micrometer.common.util.StringUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin()
 @RestController
 @RequestMapping(path = "/api/v1/admin")
 @RequiredArgsConstructor
@@ -18,6 +17,7 @@ public class AdminPageController {
 
     private final HomePageService homePageService;
 
+    @CrossOrigin()
     @PostMapping("/insert/home-page-details")
     public ResponseEntity<String> createHomePageDetails(
             @RequestBody @Valid HomeRequest homeRequest
@@ -26,5 +26,25 @@ public class AdminPageController {
         return new ResponseEntity<>("Home details saved successfully with ID: "+response, HttpStatus.CREATED);
     }
 
+    @CrossOrigin()
+    @PostMapping("/update/home-page-title/{home-id}")
+    public ResponseEntity<String> updateHomePageTitle(@PathVariable("home-id") String homeId, @RequestParam(name = "homeTitle") String homeTitle) {
+         if (StringUtils.isEmpty(homeId) || StringUtils.isEmpty(homeTitle) || StringUtils.isBlank(homeTitle) || StringUtils.isBlank(homeId)) {
+             return new ResponseEntity<>("Home details ID is required",HttpStatus.BAD_REQUEST);
+         }
+         homePageService.updateHomePageDetails(homeId, homeTitle);
+         return new ResponseEntity<>("Home details updated successfully with ID: "+homeId, HttpStatus.OK);
+    }
+
+    @CrossOrigin()
+    @PostMapping("/update/home-page-details/{home-id}")
+    public ResponseEntity<String> updateHomeDetails(@PathVariable("home-id") String homeId, @RequestBody @Valid HomeRequest homeRequest) {
+        if (StringUtils.isEmpty(homeId)) {
+            return new ResponseEntity<>("Home ID is required",HttpStatus.BAD_REQUEST);
+        }
+
+        homePageService.updateHomePageDetails(homeId, homeRequest);
+        return new ResponseEntity<>("Home details updated successfully with ID: "+homeId, HttpStatus.OK);
+    }
     // TODO: 11/23/24 updating the home details 
 }
