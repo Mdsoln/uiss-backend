1ceb937c93e13d46d6f1bb10813dfc1352c3ae71
Mdsoln <muddyfakih98@gmail.com>
2025-01-02 16:32:15 +0300

    fixed: ERROR: value too long for type character varying(9) for homeID generated

--- src/main/java/com/uiss/home/entity/Home.java
+++ src/main/java/com/uiss/home/entity/Home.java
@@ -18,7 +18,7 @@ public class Home {
     @SequenceGenerator(name = "home_generator", sequenceName = "home_seq", allocationSize = 1)
     private Integer id;
 
-    @Column(name = "home_id", nullable = false, length = 9)
+    @Column(name = "home_id", nullable = false)
     private String homeId;// automatically generated for querying and easily CRUD operations
     @Column(nullable = false)
     private String homeTitle;
